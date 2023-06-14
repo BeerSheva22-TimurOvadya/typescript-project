70e6fae9004ee230cfaa05cbcc59376888fa2210
Ovadya33 <timurgts2710@gmail.com>
2023-06-14 15:02:36 +0300

    TypeScript intro

--- build/main.js
+++ build/main.js
@@ -1,3 +1,9 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+var Rectangle_1 = __importDefault(require("./Rectangle"));
 var n = 10;
 n = 20;
 var num = 12;
@@ -12,10 +18,41 @@ var map1 = new Map([['abs', 40]]);
 console.log(map1.get('abs'));
 console.log(map1.entries());
 Array.from(map1.entries()).forEach(function (e) { return console.log("".concat(e[0], " -> ").concat(e[1], "}")); });
-var at = [];
-at[0] = 'kuku';
-at[1] = '2';
-function f(a) {
-    return typeof a == 'number' ? a * 2 : +(a + 2);
+var comp = function (num1, num2) {
+    return num1 > num2 ? 1 : -1;
+};
+displayPerson({ id: 123, date: '2000-10-10', name: 'Vasya', gender: 'female' });
+function displayPerson(prs) {
+    prs.gender && console.log(prs.gender.substring(0, 3));
 }
+function cipher(text, key) {
+    var result = '';
+    for (var i = 0; i < text.length; i++) {
+        var ascii = text.charCodeAt(i);
+        ascii += key;
+        if (ascii > 126) {
+            ascii = ascii - 95;
+        }
+        result += String.fromCharCode(ascii);
+    }
+    return result;
+}
+var a = cipher('yz', 6);
+console.log(a);
+function decipher(text, key) {
+    var result = '';
+    for (var i = 0; i < text.length; i++) {
+        var ascii = text.charCodeAt(i);
+        ascii -= key;
+        if (ascii < 126) {
+            ascii = ascii + 95;
+        }
+        result += String.fromCharCode(ascii);
+    }
+    return result;
+}
+var b = decipher(' !', 6);
+console.log(b);
+var shape = new Rectangle_1.default(3, 4);
+var width = shape.width;
 //# sourceMappingURL=main.js.map
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,9 @@
 // const f = () => console.log('Hello world');
 // f();
 
+import Rectangle from "./Rectangle";
+import Shape from "./Shape";
+
 // for (let i = 0; i < 3; i++) {
 //     setTimeout(() => console.log(`kuku ${i}`));
 // }
@@ -20,10 +23,69 @@ console.log(map1.get('abs'));
 console.log(map1.entries());
 Array.from(map1.entries()).forEach((e) => console.log(`${e[0]} -> ${e[1]}}`));
 
-let at: string[] = [];
-at[0] = 'kuku';
-at[1] = '2';
-// console.log(at);
-function f(a: string | number): number {
-    return typeof a == 'number' ? a * 2 : +(a + 2);
+// let comparator: <T> (num1: T, num2: T) => number
+// comparator = function  (num1: number, num2: number): number {
+//     return str - num;
+// }
+
+// let comparator: <T extends number>(num1: T, num2: T) => number;
+// comparator = function <T extends number>(num1: T, num2: T): number {
+//     return num1 - num2;
+// }
+
+// type Comparator<T = number> = (p1: T, p2: T) => number;
+// let comp: Comparator<string> = function (num1: string, num2: string): number {
+//     return num1 > num2 ? 1: -1;
+// };
+
+type Comparator<T> = (p1: T, p2: T) => number;
+let comp: Comparator<number> = function (num1: number, num2: number): number {
+    return num1 > num2 ? 1 : -1;
+};
+
+type Person = { id: number; date: Date | string; name: string; gender?: 'male' | 'female' };
+displayPerson({ id: 123, date: '2000-10-10', name: 'Vasya', gender: 'female'});
+function displayPerson(prs: Person): void {
+    prs.gender && console.log(prs.gender.substring(0, 3));
+    // console.log(prs.gender.substring(0, 3))
 }
+// Функция принимает какуюто стрроку и какойто ключ. функция будет делать некое кодирование(очень простое)
+// каждый код аске будет круговым сдвигом на некий сдвиг на размер ключа
+// 
+
+function cipher(text: string, key:number) : string {
+    let result = '';
+    for(let i = 0; i < text.length; i++) {
+        let ascii = text.charCodeAt(i);
+        ascii += key;
+        if(ascii > 126) {
+            ascii = ascii - 95;
+        }
+        result += String.fromCharCode(ascii);
+    }
+    return result;
+}
+
+let a = cipher('yz', 6);
+console.log(a);
+
+//decipher(" !", 6) => "yz"  
+function decipher(text: string, key:number) : string {
+    let result = '';
+    for(let i = 0; i < text.length; i++) {
+        let ascii = text.charCodeAt(i);
+        ascii -= key;
+        if(ascii < 126) {
+            ascii = ascii + 95;
+        }
+        result += String.fromCharCode(ascii);
+    }
+    return result;
+}
+let b = decipher(' !', 6);
+console.log(b);
+
+const shape: Rectangle = new Rectangle(3, 4);
+let width = shape.width;
+
+
